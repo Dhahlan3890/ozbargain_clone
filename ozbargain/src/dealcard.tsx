95ce0344c328bfbe119413a34193de8bf126c7fb
Dhahlan3890 <dhahlan2000@gmail.com>
2024-10-14 19:45:04 +0530

    fixed typo errors

--- ozbargain/src/dealcard.tsx
+++ ozbargain/src/dealcard.tsx
@@ -6,12 +6,14 @@ import {
   Avatar,
 } from "@material-tailwind/react";
 
-export function DealCard({ Title, Img, Time, comments }) {
+export function DealCard({ Title, Img, Time, comments }: { Title: string; Img: string; Time: string; comments: number }) {
   return (
     <Card
       shadow={false}
       className="relative grid h-[20rem] w-full max-w-[18rem] items-end justify-center overflow-hidden text-center"
       style={{cursor:"pointer"}}
+      placeholder={null}
+
     >
       <CardHeader
         floated={false}
@@ -19,21 +21,23 @@ export function DealCard({ Title, Img, Time, comments }) {
         color="transparent"
         className="absolute inset-0 m-0 h-full w-full rounded-none bg-cover bg-center"
         style={{ backgroundImage: `url('${Img}')` }}
+        placeholder={null}
       >
         <div className="absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50 to-transparent" />
       </CardHeader>
-      <CardBody className="relative py-2 px-2 md:px-2">
+      <CardBody className="relative py-2 px-2 md:px-2" placeholder={null}>
         <Typography
           variant="h5"
           color="white"
           className="mb-6 font-medium leading-[1.5]"
+          placeholder={null}
         >
           {Title}
         </Typography>
-        <Typography variant="h5" className="mb-4 text-gray-400">
+        <Typography variant="h5" className="mb-4 text-gray-400" placeholder={null}>
           {Time} Ago
         </Typography>
-        <Typography variant="h5" className="mb-4 text-gray-400">
+        <Typography variant="h5" className="mb-4 text-gray-400" placeholder={null}>
           {comments} comments
         </Typography>
         {/* <Avatar
--- ozbargain/src/sidebar.tsx
+++ ozbargain/src/sidebar.tsx
@@ -241,7 +241,7 @@ export function Sidebar() {
   const openDrawerLeft = () => setOpenLeft(true);
   const closeDrawerLeft = () => setOpenLeft(false);
  
-  const handleOpen = (value) => {
+  const handleOpen = (value: number) => {
     setOpen(open === value ? 0 : value);
   };
  
@@ -258,12 +258,14 @@ export function Sidebar() {
         open={openLeft}
         onClose={closeDrawerLeft}
         className="p-4"
+        placeholder={null}
       >
-    <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
+    <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5" placeholder={null}>
         <IconButton
         variant="text"
         color="blue-gray"
         onClick={closeDrawerLeft}
+        placeholder={null}
         >
         <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -285,8 +287,9 @@ export function Sidebar() {
           Sidebar
         </Typography>
       </div> */}
-      <List>
+      <List placeholder={null}>
         <Accordion
+          placeholder={null}
           open={open === 1}
           icon={
             <ChevronDownIcon
@@ -295,62 +298,62 @@ export function Sidebar() {
             />
           }
         >
-          <ListItem className="p-0" selected={open === 1}>
-            <AccordionHeader onClick={() => handleOpen(1)} className="border-b-0 p-3">
-              <ListItemPrefix>
+          <ListItem className="p-0" selected={open === 1} placeholder={null}>
+            <AccordionHeader onClick={() => handleOpen(1)} className="border-b-0 p-3" placeholder={null}>
+              <ListItemPrefix placeholder={null}>
                 <PresentationChartBarIcon className="h-5 w-5" />
               </ListItemPrefix>
-              <Typography color="blue-gray" className="mr-auto font-normal">
+              <Typography color="blue-gray" className="mr-auto font-normal" placeholder={null}>
                 Deals Categories
               </Typography>
             </AccordionHeader>
           </ListItem>
           <AccordionBody className="py-1">
-            <List className="p-0">
-              <ListItem>
-                <ListItemPrefix>
+            <List className="p-0" placeholder={null}>
+              <ListItem placeholder={null}>
+                <ListItemPrefix placeholder={null}>
                   <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                 </ListItemPrefix>
                 Automotives
               </ListItem>
-              <ListItem>
-                <ListItemPrefix>
+              <ListItem placeholder={null}>
+                <ListItemPrefix placeholder={null}>
                   <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                 </ListItemPrefix>
                 Books and magazines
               </ListItem>
-              <ListItem>
-                <ListItemPrefix>
+              <ListItem placeholder={null}>
+                <ListItemPrefix placeholder={null}>
                   <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                 </ListItemPrefix>
                 Computing
               </ListItem>
-              <ListItem>
-                <ListItemPrefix>
+              <ListItem placeholder={null}>
+                <ListItemPrefix placeholder={null}>
                   <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                 </ListItemPrefix>
                 Education
               </ListItem>
-              <ListItem>
-                <ListItemPrefix>
+              <ListItem placeholder={null}>
+                <ListItemPrefix placeholder={null}>
                   <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                 </ListItemPrefix>
                 Entertainment
               </ListItem>
-              <ListItem>
-                <ListItemPrefix>
+              <ListItem placeholder={null}>
+                <ListItemPrefix placeholder={null}>
                   <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                 </ListItemPrefix>
                 Fashion and design
               </ListItem>
-              <ListItem>
-                <ListItemPrefix>
+              <ListItem placeholder={null}>
+                <ListItemPrefix placeholder={null}>
                   <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                 </ListItemPrefix>
                 Financial
               </ListItem>
-              <ListItem>
-                <ListItemPrefix>
+              <ListItem placeholder={null}>
+                <ListItemPrefix placeholder={null}>
                   <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                 </ListItemPrefix>
                 Gaming
@@ -359,6 +362,7 @@ export function Sidebar() {
           </AccordionBody>
         </Accordion>
         <Accordion
+          placeholder={null}
           open={open === 2}
           icon={
             <ChevronDownIcon
@@ -367,68 +371,68 @@ export function Sidebar() {
             />
           }
         >
-          <ListItem className="p-0" selected={open === 2}>
-            <AccordionHeader onClick={() => handleOpen(2)} className="border-b-0 p-3">
-              <ListItemPrefix>
+          <ListItem className="p-0" selected={open === 2} placeholder={null}>
+            <AccordionHeader onClick={() => handleOpen(2)} className="border-b-0 p-3" placeholder={null}>
+              <ListItemPrefix placeholder={null}>
                 <ShoppingBagIcon className="h-5 w-5" />
               </ListItemPrefix>
-              <Typography color="blue-gray" className="mr-auto font-normal">
+              <Typography color="blue-gray" className="mr-auto font-normal" placeholder={null}>
                 Discussion Categories
               </Typography>
             </AccordionHeader>
           </ListItem>
           <AccordionBody className="py-1">
-            <List className="p-0">
-              <ListItem>
-                <ListItemPrefix>
+            <List className="p-0" placeholder={null}>
+              <ListItem placeholder={null}>
+                <ListItemPrefix placeholder={null}>
                   <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                 </ListItemPrefix>
                 Automotve
               </ListItem>
-              <ListItem>
-                <ListItemPrefix>
+              <ListItem placeholder={null}>
+                <ListItemPrefix placeholder={null}>
                   <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                 </ListItemPrefix>
                 Computing
               </ListItem>
-              <ListItem>
-                <ListItemPrefix>
+              <ListItem placeholder={null}>
+                <ListItemPrefix placeholder={null}>
                   <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                 </ListItemPrefix>
                 Education & Work
               </ListItem>
-              <ListItem>
-                <ListItemPrefix>
+              <ListItem placeholder={null}>
+                <ListItemPrefix placeholder={null}>
                   <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                 </ListItemPrefix>
                 Electrical and Electronics
               </ListItem>
-              <ListItem>
-                <ListItemPrefix>
+              <ListItem placeholder={null}>
+                <ListItemPrefix placeholder={null}>
                   <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                 </ListItemPrefix>
                 Entertainment
               </ListItem>
-              <ListItem>
-                <ListItemPrefix>
+              <ListItem placeholder={null}>
+                <ListItemPrefix placeholder={null}>
                   <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                 </ListItemPrefix>
                 Fashion
               </ListItem>
-              <ListItem>
-                <ListItemPrefix>
+              <ListItem placeholder={null}>
+                <ListItemPrefix placeholder={null}>
                   <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                 </ListItemPrefix>
                 Financial
               </ListItem>
-              <ListItem>
-                <ListItemPrefix>
+              <ListItem placeholder={null}>
+                <ListItemPrefix placeholder={null}>
                   <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                 </ListItemPrefix>
                 Gaming
               </ListItem>
-              <ListItem>
-                <ListItemPrefix>
+              <ListItem placeholder={null}>
+                <ListItemPrefix placeholder={null}>
                   <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                 </ListItemPrefix>
                 Health
@@ -437,8 +441,8 @@ export function Sidebar() {
           </AccordionBody>
         </Accordion>
         <hr className="my-2 border-blue-gray-50" />
-        {/* <ListItem>
-          <ListItemPrefix>
+        {/* <ListItem placeholder={null}>
+          <ListItemPrefix placeholder={null}>
             <InboxIcon className="h-5 w-5" />
           </ListItemPrefix>
           Inbox
@@ -446,20 +450,20 @@ export function Sidebar() {
             <Chip value="14" size="sm" variant="ghost" color="blue-gray" className="rounded-full" />
           </ListItemSuffix>
         </ListItem>
-        <ListItem>
-          <ListItemPrefix>
+        <ListItem placeholder={null}>
+          <ListItemPrefix placeholder={null}>
             <UserCircleIcon className="h-5 w-5" />
           </ListItemPrefix>
           Profile
         </ListItem>
-        <ListItem>
-          <ListItemPrefix>
+        <ListItem placeholder={null}>
+          <ListItemPrefix placeholder={null}>
             <Cog6ToothIcon className="h-5 w-5" />
           </ListItemPrefix>
           Settings
         </ListItem>
-        <ListItem>
-          <ListItemPrefix>
+        <ListItem placeholder={null}>
+          <ListItemPrefix placeholder={null}>
             <PowerIcon className="h-5 w-5" />
           </ListItemPrefix>
           Log Out
